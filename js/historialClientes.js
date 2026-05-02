03cf9c56fdfc27af5627c22a1ee40d5457175db9
Leydy Rozo <lrozo443@gmail.com>
2026-05-02 17:36:32 -0500

    crud completa, crear, leer, actualizar y eliminar

--- js/historialClientes.js
+++ js/historialClientes.js
@@ -1,32 +1,53 @@
+//variable global para usar en cualquier parte
+
 //mostrar el la vista de historial de clientes los clientes que se van guardando.
-document.addEventListener("DOMContentLoaded",  mostrarClientes); //va de primero porque dice, espera que se cargue el html y luego si ejecuta el codigo.
+document.addEventListener("DOMContentLoaded", mostrarClientes); //va de primero porque dice, espera que se cargue el html y luego si ejecuta el codigo.
 
 //creo la función que uso arriba (mostrarclientes)
-function mostrarClientes (){
-//1. recupero el array o lista de clientes desde el localStorage.
-let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+function mostrarClientes() {
+  //1. recupero el array o lista de clientes desde el localStorage.
+  let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
 
-//2.selección el body de la tabla donde van a ir los datos
-let tbody = document.querySelector("#tablaClientes");
-tbody.innerHTML = ""; //limpia la tabla antes de llenarla.
+  //2.selección el body de la tabla donde van a ir los datos
+  let tbody = document.querySelector("#tablaClientes");
+  tbody.innerHTML = ""; //limpia la tabla antes de llenarla.
 
-//3.recorrer cada cliente y crear una fila por cada uno
-clientes.forEach(cliente => {
+  //3.recorrer cada cliente y crear una fila por cada uno
+  clientes.forEach((cliente, index) => {
     let fila = document.createElement("tr");
 
     fila.innerHTML = `
-    <td>${cliente.nombres}</td>
-    <td>${cliente.apellidos}</td>
-    <td>${cliente.numDocumento}</td>
-    <td>${cliente.telefono}</td>
-    <td>${cliente.fechaEntrada}</td>
-    <td>${cliente.fechaSalida}</td>
-    <td>${cliente.numeroHab}</td>
-    `
-//especifico los campos y el orden de las columnas de la fila
+    
+        <td>${cliente.nombres}</td>
+        <td>${cliente.apellidos}</td>
+        <td>${cliente.numDocumento}</td>
+        <td>${cliente.telefono}</td>
+        <td>${cliente.fechaEntrada}</td>
+        <td>${cliente.fechaSalida}</td>
+        <td>${cliente.numeroHab}</td>
+         <td>
+          <button onclick="editarCliente(${index})">✏️</button>
+          <button onclick="borrarCliente(${index})">🗑️</button>
+        </td>
+    `;
+    //especifico los campos y el orden de las columnas de la fila
 
-//4.inserto la fila en la tabla
-tbody.appendChild(fila);
-});
+    //4.inserto la fila en la tabla
+    tbody.appendChild(fila);
+  });
+};
+
+function borrarCliente(index){
+  let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+  clientes.splice(index, 1);
+  localStorage.setItem("clientes", JSON.stringify(clientes));
+  mostrarClientes();
+};
+
+function editarCliente(index) {
+  localStorage.setItem("indiceEditando", index); //guarda el indice del cliente que quiero editar 
+  window.location.href = "regClientes.html";  //me manda al formulario con los datos en ese indice para que pueda editarlo
 }
 
+  
+  
--- js/regClientes.js
+++ js/regClientes.js
@@ -1,24 +1,71 @@
 //función para capturar los datos y guardarlos en el localStorage
+let indiceEditando = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  let indexGuardado = localStorage.getItem("indiceEditando");
+
+  if (indexGuardado !== null) {
+
+    indiceEditando = parseInt(indexGuardado);
+
+    let clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+    let cliente = clientes[indiceEditando];
+
+    // llenar formulario
+    document.getElementById("nombresReg").value = cliente.nombres;
+    document.getElementById("apellidosReg").value = cliente.apellidos;
+    document.getElementById("numDoc").value = cliente.numDocumento;
+    document.getElementById("tlf").value = cliente.telefono;
+    document.getElementById("fEntrada").value = cliente.fechaEntrada;
+    document.getElementById("fSalida").value = cliente.fechaSalida;
+    document.getElementById("tipoHab").value = cliente.numeroHab;
+  }
+});
+
 function guardar() {
-  //capturar los valores dentro de un objeto
-  const cliente = {
-    nombres: document.getElementById("nombresReg").value.trim(),
-    apellidos: document.getElementById("apellidosReg").value.trim(),
-    tipoDocumento: document.getElementById("tipDocumento").value.trim(),
-    numDocumento: document.getElementById("numDoc").value.trim(),
-    telefono: document.getElementById("tlf").value.trim(),
-    ocupacion: document.getElementById("ocupacion").value.trim(),
-    fechaEntrada: document.getElementById("fEntrada").value.trim(),
-    fechaSalida: document.getElementById("fSalida").value.trim(),
-    numeroHab: document.getElementById("tipoHab").value.trim(),
+  //capturar los valores 
+    const nombres = document.getElementById("nombresReg").value.trim();
+    const apellidos = document.getElementById("apellidosReg").value.trim();
+    const tipoDocumento = document.getElementById("tipDocumento").value.trim();
+    const numDocumento = document.getElementById("numDoc").value.trim();
+    const telefono = document.getElementById("tlf").value.trim();
+    const ocupacion = document.getElementById("ocupacion").value.trim();
+    const fechaEntrada= document.getElementById("fEntrada").value.trim();
+    const fechaSalida = document.getElementById("fSalida").value.trim();
+    const numeroHab = document.getElementById("tipoHab").value.trim();
   
-  
-  };
-  //2. recupero la lista actual de clientes (si existe)
+  //2. validar los datos antes de crear el 
+  //si hay campos obligatorios vacíos
+  if (!nombres || !apellidos || !numDocumento || !telefono || !fechaEntrada || !numeroHab) {
+    alert("⚠️ Debes completar todos los campos obligatorios*");
+    return; //se detiene y no guarda nada.
+  }
+  //si el documento tiene menos de 8 o mas de 10 dígitos
+  if
+(!/^\d{8,10}$/.test(numDocumento) || isNaN(numDocumento)) {
+    alert("⚠️ El documento debe tener entre 8 y 10 dígitos y deben ser solo números");
+    return;
+  }
+  //si el numero de telefono tiene una cantidad de digitos difetente de 10 o tiene letras
+if(telefono.length !== 10 || isNaN(telefono)){
+  alert("El número de telefono no tiene los 10 dígitos requeridos o no es número");
+  return;
+}
+  //si no hay errores, crea el objeto de clientes
   let clientes = JSON.parse(localStorage.getItem("clientes")) || []; //esta linea de codigo dice, si ya hay clientes guardados, conviertelos de texto json a un arreglo y si no hay nada, crea el arreglo vacío.
 
-  //3.agrega el nuevo cliente que se introduzca al arreglo creado
-  clientes.push(cliente); //al arreglo recien creado (clientes) insertale debajo, un nuevo (cliente).
+  const cliente = {
+    nombres, apellidos, tipoDocumento, numDocumento, telefono, ocupacion, fechaEntrada, fechaSalida, numeroHab
+  };
+
+  if (indiceEditando !== null) {
+  clientes[indiceEditando] = cliente;
+} else {
+  clientes.push(cliente);
+}
+ 
+ 
 
   //4.guarda el arreglo actualizado con el nuevo cliente en el localStorage.
   localStorage.setItem("clientes", JSON.stringify(clientes)); //convierte el arreglo completo a texto json, guardalo bajo el nombre (clientes); entonces se sobreescribe sobre el que ya está.
@@ -28,5 +75,8 @@ function guardar() {
   console.log("lista completa de clientes:", clientes); //muestrame en la consola, el cliente que acabo de guardar y la lista completa.
   document.getElementById("formClientes").reset();
   //6.recargar la pagina para limpiar el formulario
+
+  indiceEditando = null; //deja de editar clientes para luego finalizar y quedar listo para ingresar un nuevo cliente.
+
   location.reload();
-}
+};
